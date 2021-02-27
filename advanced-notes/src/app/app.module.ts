bb30ed3e81ecae6061e56d2eac7d55b73e5f014c
Giamba <g.marini@reply.it>
2021-02-27 20:12:17 +0100

    empty components

--- advanced-notes/src/app/app.module.ts
+++ advanced-notes/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { NoteBoxComponent } from './note-box/note-box.component';
+import { NoteContainerComponent } from './note-container/note-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvatarComponent,
+    NoteBoxComponent,
+    NoteContainerComponent
   ],
   imports: [
     BrowserModule
---
+++ advanced-notes/src/app/note-box/note-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-box',
+  templateUrl: './note-box.component.html',
+  styleUrls: ['./note-box.component.scss']
+})
+export class NoteBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
