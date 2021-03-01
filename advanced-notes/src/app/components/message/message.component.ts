4f3e8e02345a8c26ac95ca55a28bb01453605bc6
Giamba <g.marini@reply.it>
2021-03-02 00:10:49 +0100

    READ MORE

--- advanced-notes/src/app/components/message/message.component.ts
+++ advanced-notes/src/app/components/message/message.component.ts
@@ -1,19 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'message',
-  templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss']
+    selector: 'message',
+    templateUrl: './message.component.html',
+    styleUrls: ['./message.component.scss']
 })
 export class MessageComponent implements OnInit {
   
-  @Input() msg!: string;
-  @Input() direction!: string;
-  @Input() previewMaxLength!: number;
-  
-  constructor() { }
+    @Input() msg!: string;
+    @Input() direction!: string;
+    
+    @ViewChild('message') message: any;
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    showReadMore: boolean = false;
+
+    ngOnInit(): void {
+    }
+
+    ngAfterViewChecked(): void{
+        setTimeout(() => {
+            this.showReadMore = this.message.nativeElement.scrollHeight > this.message.nativeElement.clientHeight;
+        },0);
+    }
+
+    showText(): void{
+        this.message.nativeElement.classList.remove('line-clamp');
+    }
 
 }
